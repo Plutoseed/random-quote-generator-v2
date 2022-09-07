2ef594f49599afacea7dfcccda3f0a9864f539f0
Mekye Williams <91747261+Apolloseed@users.noreply.github.com>
2022-09-07 18:41:19 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,15 +10,19 @@ Project 4 - Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-let quotes = []; 
+let quotes = [{"Knowledge is power... and pain.": "Wraith", "Apex": "Source", "02/28/2022": "date"}, 
+{"Start before you're ready. Don't prepare, begin": "Mel Robbins"}, 
+{"There's not a single winner on Earth that took it easy": "Gary Vee"}, 
+{"There is only one thing we say to death: Not today": "Syrio Forel"}, 
+{"Always prefer the plain direct word to the long, vague one. Don't implement promises, but keep them.": "C.S. Lewis"}];
 
 
 /***
  * `getRandomQuote` function
 ***/
 function getRandomQuote() {
-
-
+  let ranNum =  Math.floor(Math.random() * quotes.length) + 1;
+  return quotes[ranNum];
 }
 
 
@@ -26,6 +30,11 @@ function getRandomQuote() {
  * `printQuote` function
 ***/
 function printQuote() {
+  function printQuote() {
+    let myq = getRandomQuote();
+  let displayq = document.querySelector('p').innerHTML = `<p class="quote"> ${myq} </p>
+  <p class="source"> ${} </p>`;
+  }
   
 }
 
