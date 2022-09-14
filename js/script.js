9ef8ec79b9e89107e49b13814d59fb920aa86e0c
Mekye Williams <91747261+Apolloseed@users.noreply.github.com>
2022-09-14 06:42:24 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,16 +8,17 @@ Project 4 - Random Quote Generator
   // Reach out in your Slack community
 
 /*** 
- * `quotes` array 
+ * HERE'S AN ARRAY OF QUOTES STORED WITHIN A VARIABLE. 
+ * EACH QUOTE IS REPRESENTED AS AN JS OBJECT  
 ***/
 let quotes = [{
-  quote : 'Hard times create strong men, strong men create good times, good times create weak men, weak men create hard times',
+  quote : 'Hard times create strong men, strong men create good times, good times create weak men, weak men create hard times.',
   source : 'G. Michael Hopf',
   citation: 'Those Who Remain',
   year: '12/06/16'
 },
 {
-  quote : 'The ability to learn faster than your competitors may be the only sustainable competitive advantage',
+  quote : 'The ability to learn faster than your competitors may be the only sustainable competitive advantage.',
   source : 'Arie de Geus', 
 },
 {
@@ -31,12 +32,12 @@ source : 'Tom Landry',
   {
     quote : 'A man without self-control is like a city broken into and left without walls.', 
     source : 'Bible', 
-    },
+    }
 ];
 
 
 /***
- * `getRandomQuote` function
+ * THIS FUNCTION RETURNS A RANDOM OBJECT WHICH HOUSES QUOTES! 
 ***/
 function getRandomQuote() {
   let ranNum =  Math.floor(Math.random() * quotes.length);
@@ -45,22 +46,23 @@ function getRandomQuote() {
 
 
 /***
- * `printQuote` function
+ * THIS FUNCTION CALLS THE GET RANDOM QUOTE FUNCTION AND DISPLAYS IT ON THE SCREEN VIA HTML
 ***/
  function printQuote() {
     let myq = getRandomQuote();
   let displayQ = `
   <p class="quote"> ${myq.quote} </p> 
-  <p class="source"> ${myq.source} </p>`;
-
+  <p class="source"> ${myq.source}`;
   if (myq.citation) {
     displayQ += ` <span class='citation'> ${myq.citation} </span>`;
+
   }
   if (myq.year) {
     displayQ += ` <span class='year'> ${myq.year} </span>`;
-  }
-  return document.getElementById('quote-box').innerHTML = `${displayQ}`; ; 
-  } ;
+  } displayQ += `</p>`;
+  return document.getElementById('quote-box').innerHTML = `${displayQ}`; 
+  }  ;
+
 
 /***
  * click event listener for the print quote button
